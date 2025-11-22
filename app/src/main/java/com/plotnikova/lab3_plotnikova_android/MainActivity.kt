8eb75c9ded5ad847bdf3a1fb4ad0ac3a44dd3618
Plotnikova <plotnikovanana@gmail.com>
2025-11-23 00:38:07 +0300

    Initial commit

---
+++ app/src/main/java/com/plotnikova/lab3_plotnikova_android/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.plotnikova.lab3_plotnikova_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.plotnikova.lab3_plotnikova_android.ui.theme.Lab3_Plotnikova_AndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PixLetterTheme {
+                GreetingImage(
+                    message = stringResource(R.string.king_message_text),
+                    from = stringResource(R.string.from_king_arthur),
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.pixelcastle)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.8F,
+            modifier = Modifier.fillMaxSize()
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp)
+        )
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .border(
+                width = 2.dp,
+                color = Color.White,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(16.dp)
+    ) {
+        InvitationTitle()
+        Text(
+            text = message,
+            fontSize = 20.sp,
+            lineHeight = 40.sp,
+            textAlign = TextAlign.Center,
+            color = Color.White
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            color = Color.White,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.End)
+        )
+    }
+}
+
+@Composable
+fun InvitationTitle(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.invitation_title),
+        fontSize = 28.sp,
+        color = Color.Yellow,
+        textAlign = TextAlign.Center,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RoyalInvitationPreview() {
+    PixLetterTheme {
+        GreetingImage(
+            message = stringResource(R.string.king_message_text),
+            from = stringResource(R.string.from_king_arthur)
+        )
+    }
+}
+
+@Composable
+fun PixLetterTheme(content: @Composable () -> Unit) {
+    TODO("Not yet implemented")
+}
